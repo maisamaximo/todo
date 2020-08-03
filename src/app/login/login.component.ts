96ab18d9bcde4fa7aa5db952fd1721242020e55f
Maisa Maximo <maisa_maximo@outlook.com>
2020-08-03 12:11:13 -0300

    step 13 adding click event

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
+  username = "maisamaximo";
+  password = "123";
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  handleLogin() {
+    console.log(this.username);
   }
-
 }
